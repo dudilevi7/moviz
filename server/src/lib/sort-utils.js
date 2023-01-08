b80b45dcf64b6ebd9e89ec820b5af2f652b8d301
David Levi <dlevis10@gmail.com>
2023-01-08 18:10:16 +0200

    * add informative logs, * handle few critical points on data

--- server/src/lib/sort-utils.js
+++ server/src/lib/sort-utils.js
@@ -1,6 +1,6 @@
 const lexicalComparison = (a = '', b = '') => {
-    let x = a.toLowerCase();
-    let y = b.toLowerCase();
+    let x = a?.toLowerCase();
+    let y = b?.toLowerCase();
     if (x < y) { return -1; }
     if (x > y) { return 1; }
     return 0;
@@ -9,10 +9,10 @@ const arrayComparsion = (a = [], b = []) => a.length - b.length
 const numberComparsion = (a = 0, b = 0) => a - b
 
 const columnSort = {
-    rank: (a, b) => numberComparsion(a.rank, b.rank),
+    rank: (a, b) => numberComparsion(parseInt(a.rank), parseInt(b.rank)),
     title: (a, b) => lexicalComparison(a.title, b.title),
     year: (a, b) => numberComparsion(a.year, b.year),
-    director: (a, b) => lexicalComparison(a.director, b.director),
+    director: (a, b) => lexicalComparison(a?.director, b?.director),
     actors: (a, b) => arrayComparsion(a.actors, b.actors)
 }
 
--- server/src/routes/movies/getMovies.js
+++ server/src/routes/movies/getMovies.js
@@ -8,28 +8,33 @@ const getMovies = async (req, res) => {
             colSort = '',
             colOrder = 'asc',
             pageNum = 1,
-            pageSize = 7,
+            pageSize = 10,
             search
         } = req.query
 
         let movies = [...data]
         if (search) {
-            movies = movies.filter((elem) => Object.values(elem).find((value) => value && value.toString().toLowerCase().includes(search.toLowerCase())))
+            console.log(`filtering movies by input ${search}`)
+            movies = movies.filter(({ id, ...movie }) => Object.values(movie).find((value) => value && value.toString().toLowerCase().includes(search.toLowerCase())))
         }
-        if (colSort) {
-            movies = movies.sort((a, b) => colOrder === 'asc' ? columnSort[colSort](a, b) : columnSort[colSort](b, a))
-        }
-        const pageNumber = parseInt(pageNum)
+
+        const availableMovies = movies.length
         const rowsPerPage = parseInt(pageSize)
+        const pageNumber = parseInt(availableMovies <= rowsPerPage ? 1 : pageNum)
         const startIndex = (pageNumber - 1) * rowsPerPage
         const endIndex = pageNumber * rowsPerPage
-        const slicedData = movies.slice(startIndex, endIndex)
+        movies = movies.slice(startIndex, endIndex)
+
+        if (colSort) {
+            console.log(`sorting movies by column: ${colSort} in order: ${colOrder} order`)
+            movies = movies.sort((a, b) => colOrder === 'asc' ? columnSort[colSort](a, b) : columnSort[colSort](b, a))
+        }
 
         console.log('mapping movies to row data and columns')
-        const mappedMovies = mapMoviesToTable(slicedData)
+        const mappedMovies = mapMoviesToTable(movies)
 
         console.log('returning movies data')
-        return res.json(mappedMovies)
+        return res.json({ ...mappedMovies, availableMovies, pageNum, pageSize, search, colSort, colOrder })
     } catch (err) {
         console.error('failed to get movies', err)
         return res.status(500).json({ message: err.message })
