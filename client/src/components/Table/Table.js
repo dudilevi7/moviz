83dd486dd8d8fc529fa71ea0cf24a82bbdde7587
David Levi <dlevis10@gmail.com>
2023-01-08 21:37:00 +0200

    handle error in init

--- client/src/components/Table/Table.js
+++ client/src/components/Table/Table.js
@@ -52,7 +52,7 @@ const Table = () => {
                     }, 500)
                 }
                 else {
-                    setError()
+                    setError(true)
                 }
             }
             initializeData()
